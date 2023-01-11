42828cbf00157c65e8b40bb4b7d2aacd5e565daa
cole <57602082+cole-cyber@users.noreply.github.com>
2023-01-11 08:30:01 -0600

    Update HelloWorld.vue
    updated endpoint

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,7 +26,7 @@ const MAX_IMAGE_SIZE = 1000000
 /* ENTER YOUR ENDPOINT HERE
    FILES UPLOADED TO MY ENDPOINT ARE AUTOMATICALLY DELETED EVERY FEW HOURS */
 
-const API_ENDPOINT = 'https://9ajmohb40l.execute-api.us-east-1.amazonaws.com/default/MyRegistrationForm'
+const API_ENDPOINT = ' https://w2vrxgzro5.execute-api.us-east-1.amazonaws.com/default/serverlessLambda'
 
 export default {
   name: 'app',
