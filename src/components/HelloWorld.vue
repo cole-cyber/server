a6eb04e3601820824bf2834ce8ebeb1d0f2ac663
cole <57602082+cole-cyber@users.noreply.github.com>
2023-01-12 01:54:30 -0600

    updated Hello World
    My new api endpoint is added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,7 +26,7 @@ const MAX_IMAGE_SIZE = 1000000
 /* ENTER YOUR ENDPOINT HERE
    FILES UPLOADED TO MY ENDPOINT ARE AUTOMATICALLY DELETED EVERY FEW HOURS */
 
-const API_ENDPOINT = ' https://w2vrxgzro5.execute-api.us-east-1.amazonaws.com/default/serverlessLambda'
+const API_ENDPOINT = 'https://rauxenffp2.execute-api.us-east-1.amazonaws.com/default/colelambda'
 
 export default {
   name: 'app',
